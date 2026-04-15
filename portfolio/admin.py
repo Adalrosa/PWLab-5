792e4db90ad147622c35c7cd80f2c1be6a4da549
lp1 <160870127+Adalrosa@users.noreply.github.com>
2026-04-15 02:33:55 +0000

    Add pasta project

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -3,3 +3,9 @@ from .models import Licenciatura, UnidadeCurricular
 
 admin.site.register(Licenciatura)
 admin.site.register(UnidadeCurricular)
+from django.contrib import admin
+from .models import Licenciatura, UnidadeCurricular, Projecto  # Adiciona o Projecto aqui
+
+admin.site.register(Licenciatura)
+admin.site.register(UnidadeCurricular)
+admin.site.register(Projecto)  
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class Licenciatura(models.Model):
  nome=models.CharField(max_length=100)
  apresentação=models.TextField()
@@ -7,3 +8,8 @@ class UnidadeCurricular(models.Model):
  nome= models.CharField(max_length=100)
  ano=models.IntegerField()
  licenciatura= models.ForeignKey(Licenciatura, on_delete=models.CASCADE)
+
+class Projecto(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    unidade_curricular = models.ForeignKey(UnidadeCurricular, on_delete=models.CASCADE)
