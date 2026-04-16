70528b50e66b0a1e5883f974d0e8717931e4ad17
lp1 <160870127+Adalrosa@users.noreply.github.com>
2026-04-16 05:24:41 +0000

    implementação da arquitetura MVT e página inicial dinâmica

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Projecto, Competencia
 
-# Create your views here.
+# Verifica se o nome aqui é EXATAMENTE igual a home_page_view
+def home_page_view(request):
+    context = {
+        'projetos': Projecto.objects.all(),
+        'competencias': Competencia.objects.all(),
+    }
+    return render(request, 'portfolio/home.html', context)
